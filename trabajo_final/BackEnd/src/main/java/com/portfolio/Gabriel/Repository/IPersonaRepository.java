7f616de5c19137c2c8a4473a07b489d567011aba
GabrielYN <gabrielgonzaleza320915@gmail.com>
2023-05-13 11:06:32 -0300

    Implementacion del backend en el proyecto

---
+++ trabajo_final/BackEnd/src/main/java/com/portfolio/Gabriel/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.Gabriel.Repository;
+
+import com.portfolio.Gabriel.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long>{
+    
+}
---
+++ trabajo_final/BackEnd/src/main/java/com/portfolio/Gabriel/Security/Enums/RolNombre.java
@@ -0,0 +1,7 @@
+
+package com.portfolio.Gabriel.Security.Enums;
+
+
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+}
